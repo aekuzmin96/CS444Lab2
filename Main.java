1e49d2a193f9d3728e36351fb991b6be040ab265
Anton Kuzmin <akuzmin@unm.edu>
2017-03-21 17:02:14 -0600

    hex array

--- Main.java
+++ Main.java
@@ -1,5 +1,39 @@
 
 public class Main {
+    /*
+     4b4d3239764863686245437379465942
+     adbeb300136c6305bf21eb69dc71e7c0
+     0b9c6f19dd789479e884d29da45c0bea
+     c49d4a5701c2a903e9ce0d59228ea4d0
+     f003a065a9ac9e04f3330ef903be54dc
+     d7307dbc20d96bc3a3e3b45c6af1447f
+     fe86cdeba431347e6f27a88015a69dc0
+     9565754cdc87df46697fbd98184a07e5
+     b807a08ac6f3561650c6df665c4a7758
+     746fe7b340a22a6d01d94fa8b95d983d
+     210ea4e367b5f961cc93407c9b0598a6
+     */
+
+    private static String IV = "4b4d3239764863686245437379465942";
+    private static String ciphertext = "adbeb300136c6305bf21eb69dc71e7c0";
+    private static String[] rValue = new String[16];
+    private static String[] hex = new String[256];
+    private static int[] offset = new int[16];
+
+    private static void buildHexValues()
+    {
+        for(int i = 0; i < 256; i++)
+        {
+            if(i < 16)
+            {
+                hex[i] = "0" + Integer.toHexString(i);
+            }
+            else
+            {
+                hex[i] = Integer.toHexString(i);
+            }
+        }
+    }
 
     private static String xor(String one, String two)
     {
@@ -10,6 +44,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-
+        buildHexValues();
+        System.out.println(hex[255]);
     }
 }
