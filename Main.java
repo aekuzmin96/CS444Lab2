252053805013dc5b434dd24139bb324e64882744
Anton Kuzmin <akuzmin@unm.edu>
2017-03-21 16:50:32 -0600

    xor function

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 
 public class Main {
+
+    private static String xor(String one, String two)
+    {
+        int a = Integer.parseInt(one, 16);
+        int b = Integer.parseInt(two, 16);
+        return Integer.toString(a ^ b, 16);
+    }
+
     public static void main(String[] args)
     {
 
