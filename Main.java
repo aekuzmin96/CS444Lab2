be69d229639ade624f545023a068685214d80550
Anton Kuzmin <akuzmin@unm.edu>
2017-03-23 18:02:30 -0600

    Everything done!

--- Main.java
+++ Main.java
@@ -16,22 +16,11 @@ public class Main {
 
      python client.py -ip shasta.cs.unm.edu -p 10035 -b 'IV+CT' -id 35
      */
-    /*private static String[] pcap = {"4b4d3239764863686245437379465942",
-            "adbeb300136c6305bf21eb69dc71e7c0",
-            "0b9c6f19dd789479e884d29da45c0bea",
-            "c49d4a5701c2a903e9ce0d59228ea4d0",
-            "f003a065a9ac9e04f3330ef903be54dc",
-            "d7307dbc20d96bc3a3e3b45c6af1447f",
-            "fe86cdeba431347e6f27a88015a69dc0",
-            "9565754cdc87df46697fbd98184a07e5",
-            "b807a08ac6f3561650c6df665c4a7758",
-            "746fe7b340a22a6d01d94fa8b95d983d",
-            "210ea4e367b5f961cc93407c9b0598a6"};*/
     private static String path = "python /nfs/student/a/akuzmin/IdeaProjects/CS444Lab2/src/client.py ";
     private static String arg1 = "-ip shasta.cs.unm.edu -p 10035 -b ";
     private static String arg2 = " -id 35";
-    private static String IV = "adbeb300136c6305bf21eb69dc71e7c0";
-    private static String ciphertext = "0b9c6f19dd789479e884d29da45c0bea";
+    private static String IV = "746fe7b340a22a6d01d94fa8b95d983d";
+    private static String ciphertext = "210ea4e367b5f961cc93407c9b0598a6";
     private static String[] disassembledIV;
     private static String[] rValue = new String[16];
     private static String[] hex = new String[256];
